e9c127364c199a1ec20fa23d915ecaaa2db2086a
nikhil8969 <119287650+nikhil8969@users.noreply.github.com>
2025-01-20 01:09:03 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Three important lesson of life:</h1>
+    <ul>
+      <li>Never trust anyone.</li>
+      <li>Nothing is more important than money.</li>
+      <li>Family is most important thing in life.</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
